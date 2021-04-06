4e2e55fe9a5087df5b9f03c623185d0a27d13a09
Sebastian Reimers <hallo@studio-link.de>
2021-04-06 08:31:47 +0200

    webui: update tailwindcss 2.1.0 and jit

--- webui/tailwind.config.js
+++ webui/tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
