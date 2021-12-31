4bf743c1faadd05e10fb1aceed39ddb015beec80
Sebastian Reimers <hallo@studio-link.de>
2021-12-31 08:44:10 +0100

    webui: update npm and migrate tailwindcss v3

--- webui/tailwind.config.js
+++ webui/tailwind.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  mode: "jit",
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     container: {
       screens: {
