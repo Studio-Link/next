0e15ec9ac6b87435d10fd733029ed22336f94cd9
Sebastian Reimers <hallo@studio-link.de>
2022-09-11 12:34:56 +0200

    webui: fix bottom action hover state

--- webui/src/components/BottomActions.vue
+++ webui/src/components/BottomActions.vue
@@ -1,39 +1,33 @@
 <template>
-  <div class="flex justify-center space-x-5 sm:space-x-16 mb-5 mx-auto">
-    <button
-      :class="{ 'bg-red-900 hover:bg-red-800': muted, 'bg-sl-08dp': !muted}"
-      class="rounded-full h-20 w-20 flex items-center justify-center hover:bg-gray-700 shadow uppercase font-semibold"
-      @click="muted = !muted"
-    >
-      Mute
-    </button>
-    <button
-      :class="{ 'bg-red-900 hover:bg-red-800': record, 'bg-sl-08dp': !record}"
-      class="rounded-full h-20 w-20 flex items-center justify-center hover:bg-gray-700 shadow uppercase font-semibold"
-      @click="record = !record"
-    >
-      Record
-    </button>
-    <button
-      :class="{ 'bg-red-900 hover:bg-red-800': chat, 'bg-sl-08dp': !chat}"
-      class="rounded-full h-20 w-20 flex items-center justify-center hover:bg-gray-700 shadow uppercase font-semibold"
-      @click="chat = !chat"
-    >
-     Chat
-    </button>
-  </div>
+    <div class="flex justify-center space-x-5 sm:space-x-16 mb-5 mx-auto">
+        <button :class="[muted ? 'bg-red-900 hover:bg-red-800' : 'bg-sl-08dp hover:bg-neutral-700']"
+            class="rounded-full h-20 w-20 flex items-center justify-center shadow uppercase font-semibold"
+            @click="muted = !muted">
+            Mute
+        </button>
+        <button :class="[record ? 'bg-red-900 hover:bg-red-800' : 'bg-sl-08dp hover:bg-neutral-700']"
+            class="rounded-full h-20 w-20 flex items-center justify-center shadow uppercase font-semibold"
+            @click="record = !record">
+            Record
+        </button>
+        <button :class="[chat ? 'bg-green-900 hover:bg-green-800' : 'bg-sl-08dp hover:bg-neutral-700']"
+            class="rounded-full h-20 w-20 flex items-center justify-center shadow uppercase font-semibold"
+            @click="chat = !chat">
+            Chat
+        </button>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  data() {
-    return {
-      muted: false,
-      record: false,
-      chat: false
-    };
-  },
+    data() {
+        return {
+            muted: false,
+            record: false,
+            chat: false
+        };
+    },
 });
 </script>
