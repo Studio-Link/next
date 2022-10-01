bed4d6d7c70c2aa9b1888cf355a6b658d1cf6b52
Sebastian Reimers <hallo@studio-link.de>
2022-10-01 09:01:01 +0200

    webui: cleanup

--- webui/src/main.ts
+++ webui/src/main.ts
@@ -9,45 +9,8 @@ import { config } from "./config";
 import './shortcuts'
 
 tracks.websocket(config.ws_host());
-let handleClickOutside: any;
 
 const app = createApp(App);
-app.directive("click-outside", {
-/*
-    beforeMount(el, binding) {
-        handleClickOutside = (e: any) => {
-            e.stopPropagation();
-            const { handler, exclude } = binding.value;
-            let clickedOnExcludedEl = false;
-
-            // Gives you the ability to exclude certain elements if you want,
-            // pass as array of strings to exclude
-            if (exclude) {
-                exclude.forEach((refName: string) => {
-                    if (!clickedOnExcludedEl) {
-                        if (binding != null && binding.instance != null) {
-                            const excludedEl: any = binding.instance.$refs[refName];
-                            if (excludedEl !== null) {
-                                clickedOnExcludedEl = excludedEl.contains(e.target);
-                            }
-                        }
-                    }
-                });
-            }
-
-            if (!el.contains(e.target) && !clickedOnExcludedEl) {
-                handler(e);
-            }
-        };
-        document.addEventListener("click", handleClickOutside);
-        document.addEventListener("touchstart", handleClickOutside);
-    },
-    unmounted() {
-        document.removeEventListener("click", handleClickOutside);
-        document.removeEventListener("touchstart", handleClickOutside);
-    },
-*/
-});
 app.component("ButtonPrimary", ButtonPrimary);
 app.component("ButtonSecondary", ButtonSecondary);
 app.mount("#app");
