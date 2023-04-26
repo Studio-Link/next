30e41711c5d4ef48fd27773c35f4612a10852e92
Sebastian Reimers <hallo@studio-link.de>
2023-04-26 15:29:43 +0200

    webui: fix nav mockup

--- webui/src/components/StudioNav.vue
+++ webui/src/components/StudioNav.vue
@@ -30,21 +30,6 @@
                         </a>
                         <hr class="w-2/3 border-t-white/[.06] border-t-2 rounded mx-2" />
                         <a href="#" class="relative block group">
-                            <div class="absolute flex items-center h-full -left-2">
-                                <div
-                                    class="h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-sl-primary rounded-r">
-                                </div>
-                            </div>
-                            <div class="group-active:translate-y-px">
-                                <div
-                                    class="text-gray-100 group-hover:rounded-2xl group-hover:bg-brand group-hover:text-white bg-sl-02dpa rounded-3xl flex items-center justify-center w-12 h-12 transition-all duration-200 overflow-hidden">
-                                    <img alt="Studio Metaebene" class="rounded-lg"
-                                        src="https://mein.podstock.de/storage/small/logos/7GakpLlUWvPYH6NlzHEBD88wZiI6TnKRc1hX56Ds.png" />
-                                    <span class="sr-only">Podstock</span>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="#" class="relative block group">
                             <div class="absolute flex items-center h-full -left-4">
                                 <div
                                     class="h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100 w-1 transition-all duration-200 origin-left bg-sl-primary rounded-r">
