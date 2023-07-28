883520a63d6d628fa1c09ea968d23488137ac3a7
Sebastian Reimers <hallo@studio-link.de>
2023-07-28 08:35:58 +0200

    bump openssl version

--- versions.mk
+++ versions.mk
@@ -1,4 +1,4 @@
-OPENSSL_VERSION   := 3.1.0
+OPENSSL_VERSION   := 3.1.1
 OPENSSL_MIRROR    := https://www.openssl.org/source
 OPUS_VERSION      := 1.4
 OPUS_MIRROR       := https://downloads.xiph.org/releases/opus
