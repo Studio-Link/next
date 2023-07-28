0efe42a4d5aa81f91e3faeadf7f82473a3f9ac53
Sebastian Reimers <hallo@studio-link.de>
2023-07-28 12:58:44 +0200

    update version

--- versions.mk
+++ versions.mk
@@ -1,6 +1,8 @@
 OPENSSL_VERSION   := 3.1.1
 OPENSSL_MIRROR    := https://www.openssl.org/source
 OPUS_VERSION      := 1.4
+H264_MIRROR       := https://github.com/cisco/openh264/archive/refs/tags
+H264_VERSION      := 2.3.1
 FFMPEG_MIRROR     := https://ffmpeg.org/releases
 FFMPEG_VERSION    := 6.0
 OPUS_MIRROR       := https://downloads.xiph.org/releases/opus
@@ -9,4 +11,4 @@ PORTAUDIO_MIRROR  := https://github.com/studio-link-3rdparty
 SAMPLERATE_MIRROR := https://github.com/studio-link-3rdparty
 LIBRE_VERSION     := main
 BARESIP_VERSION   := main
-CACHE_VERSION     := 9 # github cache invalidation workaround
+CACHE_VERSION     := 0 # github cache invalidation workaround
