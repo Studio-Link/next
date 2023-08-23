e62196c981ac6436ff43018e8e8a46d47451cd6c
Sebastian Reimers <hallo@studio-link.de>
2023-08-23 14:20:22 +0200

    cmake: change FindLMDB args order

--- cmake/FindLMDB.cmake
+++ cmake/FindLMDB.cmake
@@ -2,6 +2,6 @@ find_path(LMDB_INCLUDE_DIR NAMES  lmdb.h HINTS include)
 find_library(LMDB_LIBRARIES NAMES lmdb   HINTS lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LMDB DEFAULT_MSG LMDB_INCLUDE_DIR LMDB_LIBRARIES)
+find_package_handle_standard_args(LMDB DEFAULT_MSG LMDB_LIBRARIES LMDB_INCLUDE_DIR)
 
 mark_as_advanced(LMDB_INCLUDE_DIR LMDB_LIBRARIES)
