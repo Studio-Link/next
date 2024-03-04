0d97f6eee06c4238c41d523ff6d97efd8a75e54b
Sebastian Reimers <hallo@studio-link.de>
2024-03-04 13:49:55 +0100

    remove openh264

--- cmake/FindOPENH264.cmake
+++
@@ -1,6 +0,0 @@
-find_library(OPENH264_LIBRARIES NAMES openh264 HINTS lib)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OPENH264 DEFAULT_MSG OPENH264_LIBRARIES)
-
-mark_as_advanced(OPENH264_LIBRARIES)
