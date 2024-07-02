5979f9b93c2668f9ed076eace0dd31d971a53897
Sebastian Reimers <hallo@studio-link.de>
2024-07-02 11:29:54 +0200

    ci/macos: fix libtool and disable integration test (#19)
    * ci/macos: add libtool
    
    * enable trace
    
    * bump websocat and add curl
    
    * debug curl
    
    * remove error pipe
    
    * use brew curl
    
    * disable integration test
    
    * ci/linux: add integration test

--- test/integration.sh
+++ test/integration.sh
@@ -6,7 +6,7 @@ set -o errexit  # Exit on most errors
 set -o nounset  # Disallow expansion of unset variables
 set -o errtrace # Make sure any error trap is inherited
 set -o pipefail # Use last non-zero exit code in a pipeline
-# set -o xtrace   # Trace the execution of the script
+set -o xtrace   # Trace the execution of the script
 
 IFS=$'\n\t'
 
@@ -71,7 +71,7 @@ script_init() {
 }
 
 curl_head() {
-	curl -I "${test_url}$1" 2>/dev/null
+	curl -I "${test_url}$1"
 }
 
 curl_post() {
