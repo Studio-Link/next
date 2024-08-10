5bf21aa7e7c55da68423877678921c35836ef33a
Sebastian Reimers <hallo@studio-link.de>
2024-08-10 17:12:45 +0200

    request permissions and add opensles module

--- app/android/app/src/main/java/link/studio/app/MainActivity.kt
+++ app/android/app/src/main/java/link/studio/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package link.studio.app
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebResourceError
@@ -9,6 +10,8 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 private var refused_fail = 0
 
@@ -43,6 +46,13 @@ private class MyWebViewClient : WebViewClient() {
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions,0)
+        }
+
         Api.slStart(filesDir.absolutePath)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
